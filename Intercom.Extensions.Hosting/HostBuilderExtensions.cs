301a42dfabb229ac04f628328650a685d495917b
Erik Hennerfors <erik.hennerfors@laget.se>
2023-06-27 15:51:40 +0200

    Initial commit

---
+++ Intercom.Extensions.Hosting/HostBuilderExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Intercom.Constants;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Intercom.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseIntercom(this IHostBuilder builder, string baseUrl, string bearerToken)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            ConfigureIntercom(baseUrl, bearerToken);
+
+            return builder;
+        }
+
+        public static IHostBuilder UseIntercom(this IHostBuilder builder, string bearerToken)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            ConfigureIntercom(Url.Production, bearerToken);
+
+            return builder;
+        }
+
+        public static IHostBuilder UseCronitor(this IHostBuilder builder, Func<HostBuilderContext, string> options)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            builder.ConfigureServices((context, services) =>
+            {
+                var apiKey = options(context);
+                ConfigureIntercom(apiKey);
+            });
+
+            return builder;
+        }
+
+        private static void ConfigureIntercom(string baseUrl, string bearerToken)
+        {
+            if (string.IsNullOrWhiteSpace(bearerToken)) throw new ArgumentNullException(nameof(bearerToken));
+
+            Intercom.Configure(baseUrl, bearerToken);
+        }
+    }
+
+    public class IntercomOptions
+    {
+        public string Url { get; set; }
+        public string BearerToken { get; set; }
+
+        public IntercomOptions(string url, string bearerToken)
+        {
+            Url = url;
+            BearerToken = bearerToken;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# Intercom.Extensions.Hosting
+Hosting and startup abstractions for Intercom. When using NuGet 3.x this package requires at least version 3.4....
+
+## Usage
+```c#
+await Host.CreateDefaultBuilder()
+    .UseIntercom("apiKey")
+    .Build()
+    .RunAsync();
+```
+
+
+```c#
+await Host.CreateDefaultBuilder()
+    .UseIntercom((context) => context.Configuration.GetValue<string>("Cronitor:ApiKey"))
+    .Build()
+    .RunAsync();
+```
